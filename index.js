5d0892302c603eb7029ef378002c941f10bcb6e9
LiamDonohue <liamd@naturallygrownkids.org>
2020-04-27 00:00:03 +0000

    sdf

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 const fs = require("fs");
-let output = fs.readFileSync("economy.json", "utf8");
-let data = JSON.parse(output);
+let output = fs.readFileSync("economy.txt", "utf8");
 
-console.log(data);
 
 const express = require('express');
 const app = express();
@@ -21,32 +19,80 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-client.on('guildMemberAdd', member=>{
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+
+
+/*client.on('guildMemberAdd', member=>{
+  const channel = member.guild.channels.cache.find(ch => ch.name === '#welcome');
   
   if(!channel) return;
 
   channel.send('Welcome to the server, ${member}');
-});
+});*/
+
+
 client.on('message', message => {
-  // Here is where you need to code
-  if(message.content == "n!help") {
+  var commands = ["n!help", "n!ping", "n!github", "n!invite"];
+  var prefix = "n!";
+  if (message.content.startsWith(prefix)){
+   var args = message.content.split(' ').slice(1);
+   var allArgs = args.join(' ');
+   var cmd = (message.content.split(' ')[0]).substring(prefix.length).toLowerCase();
+
+   if (cmd === "help"){
+     message.channel.send("**The Current List Of Commands:**\n " + commands.join("\n "));
+     //console.log("There was a client request");
+   }
+   
+   else if (cmd === "ping"){
+     message.channel.send("Pong!");
+    //console.log("There was a client request");
+   }
+   else if (cmd === "github"){
+    message.channel.send("https://github.com/LiamDonohue/Nebula-Discord-Bot");
+    //console.log("There was a client request");
+   }
+   else if (cmd === "invite"){
+     message.channel.send("https://discordapp.com/oauth2/authorize?client_id=703378972335145041&permissions=402849799&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize&scope=bot");
+     //console.log("There was a client request");
+   }
+   else if (cmd === "eval"){
+     try{
+     eval(allArgs)
+     }
+     catch(err){throw err}
+     
+   }
+  /* else if (cmd === "readtxt"){
+     message.channel.send(output);
+   }*/
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    /*if(message.content == "help") {
     message.channel.send("```\nHere is the list of commands:\n 1. n!github: Displays GitHub link\n 2. n!ping : pings the bot\n 3. n!invite: sends the invite link\n```");
-  }
-  else if (message.content == "n!ping"){
+   }
+    else if (message.content == "ping"){
     message.channel.send("Pong!");
   }
-  else if(message.content == "n!github"){
+   else if(message.content == "github"){
     message.channel.send("https://github.com/LiamDonohue/Nebula-Discord-Bot");
   }
-  else if(message.content == "n!invite"){
+    else if(message.content == "invite"){
     message.channel.send("https://discordapp.com/oauth2/authorize?client_id=703378972335145041&permissions=402849799&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Fapi%2Foauth2%2Fauthorize&scope=bot");
-  }
-  else if(message.content == "n!data")
-  {
-    console.log("I'm Controlling this from discord!")
-  }
-  
+  }*/
+
+}
 });
 
 client.login(process.env.token);
